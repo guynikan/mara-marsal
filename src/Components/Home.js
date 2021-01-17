db9e2a2c198005f4623a3aa00deada4cc8db6853
Ed <jr-lol@live.com>
2021-01-17 13:58:45 -0300

    criada página home

---
+++ src/Components/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "./Header";
+import styles from "../Styles/Home.module.css";
+
+function Home() {
+  return (
+    <>
+      <Header />
+    </>
+  );
+}
+
+export default Home;
