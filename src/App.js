af56a2a9cb75ed6200a67508d930f04c1b9eb198
Ed <jr-lol@live.com>
2021-01-17 13:59:59 -0300

    retirado código desnecessário

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import "./Styles/reset.css";
+import "./App.css";
+import Home from "./Components/Home";
+import EnterGallery from "./Components/EnterGallery";
+import Footer from "./Components/Footer";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Home />
+      <EnterGallery />
+      <Footer />
+    </>
   );
 }
 
