e14e1b55f20f989352a3519de48d4d8dc0b78d56
Ed <jr-lol@live.com>
2021-01-17 13:58:01 -0300

    criado footer

---
+++ src/Components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div style={{ textAlign: "center" }}>
+      <p>All photos © Mara Marsal, 2020</p>
+    </div>
+  );
+}
+
+export default Footer;
